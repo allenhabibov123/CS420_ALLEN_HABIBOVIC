0811a436ca2d83a99d375642caf3ab0d723a7538
Allen Habibovic <ahabibov@neiu.edu>
2021-07-11 16:18:10 -0500

    Threads and Concurrency Allen Habibovic CS 420
    ALL CHECKS PASS

--- ThreadsAndConcurrency/RacingCars/src/Racer.java
+++ ThreadsAndConcurrency/RacingCars/src/Racer.java
@@ -1,11 +1,30 @@
-public class Racer /* TODO 
+public class Racer implements Runnable/* TODO
 Add something here so that you can pass this Racer to the constructor of Thread*/{
     Car c;
     int distance = 1000; // this is a racer that is going to run 1000 miles.
 
     /* TODO 
 Create a constructor that receives a Car objec and initializes c.*/
+    public Racer(Car c) {
+        this.c = c;
+    }
+
     @Override
+    public void run(){
+        while(c.odometer < distance) {
+            if(c.miles == 0){
+                System.out.println("Charging");
+                 c.miles = c.maxMiles;
+                try {
+                    Thread.sleep((long) c.maxMiles);
+                }catch(InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }else {
+                c.move(1);
+            }
+        }
+    }
     /* TODO 
 Override one method.  This is the task the thread will perform... You have to determine which method this is.
 Check the method description. */
--- ThreadsAndConcurrency/RacingCars2/src/Racer.java
+++ ThreadsAndConcurrency/RacingCars2/src/Racer.java
@@ -1,3 +1,35 @@
-public class Racer /* TODO 
+ /* TODO
 This class is exactly as the Racer from Task 1*/
+public class Racer implements Runnable {/* TODO
+Add something here so that you can pass this Racer to the constructor of Thread*/
+    Car c;
+    int distance = 1000; // this is a racer that is going to run 1000 miles.
+
+    /* TODO
+Create a constructor that receives a Car objec and initializes c.*/
+    public Racer(Car c) {
+        this.c = c;
+    }
+
+    @Override
+    public void run(){
+        while(c.odometer < distance) {
+            if(c.miles == 0){
+                System.out.println("Charging");
+                c.miles = c.maxMiles;
+                try {
+                    Thread.sleep((long) c.maxMiles);
+                }catch(InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }else {
+                c.move(1);
+            }
+        }
+    }
+    /* TODO
+Override one method.  This is the task the thread will perform... You have to determine which method this is.
+Check the method description. */
 }
+
+
